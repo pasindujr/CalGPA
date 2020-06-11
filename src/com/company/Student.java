4e00a42d409e648aa4c559dc613e7a29a8bc6530
Pasindu <pasindujayanada@gmail.com>
2020-06-11 11:43:51 +0530

    ignore specific files

---
+++ src/com/company/Student.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Student {
+    String name;
+    double gpa;
+    String subject;
+    int marksjava;
+    int marksdata;
+    int marksstat;
+    int markseng;
+    int marksflash;
+    int markssad;
+    int marksethics;
+    int marksdcn;
+
+    public void getGrade() {
+        
+
+    }
+}
+
